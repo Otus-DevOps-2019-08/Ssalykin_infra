2da12869c23401b0032d0dacf148450da4b3e96d
Ssalykin <wazuk92@gmail.com>
2019-11-06 16:03:52 +0300

    HW9

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+project          = "studious-saga-254012"
+public_key_path  = "~/.ssh/appuser.pub"
+disk_image       = "reddit-base-1572253503"
+private_key_path = "~/.ssh/appuser"
+db_disk_image = "reddit-db-base-1572609216"
+app_disk_image = "reddit-app-base-1572608933"               
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,19 +6,3 @@ variable region {
   # Значение по умолчанию
   default = "europe-west1"
 }
-variable public_key_path {
-  # Описание переменной
-  description = "Path to the public key used for ssh access appuser"
-}
-variable disk_image {
-  description = "Disk image"
-}
-variable private_key_path {
-  # Описание переменной
-  description = "Path to the private key used for ssh access appuser"
-}
-variable machine_type {
-  description = "machine type"
-  # Значение по умолчанию
-  default = "g1-small"
-}
